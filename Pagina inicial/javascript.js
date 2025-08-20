742f37bf6789b61d2cca4cc9765e6f9f0a52418e
Rafael Nunes Cardoso <rfnunes1000@gmail.com>
2025-08-20 11:42:52 -0300

    update
    Adicionei as avaliações mas precisa arrumar algumas coisas

--- Pagina inicial/javascript.js
+++ Pagina inicial/javascript.js
@@ -1,1 +1,36 @@
 
+//===================== Testmonial Swiper =====================
+const swiperAvaliacoes = new Swiper('.avaliador__swiper', {
+  loop: true,
+  slidesPerView: 'auto',
+  centeredSlides: 'auto',
+  spaceBetween: 16,
+  grapCursor: true,
+  speed: 600,
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate:-90,
+    depth: 800,
+    modifier: .5,
+    slideShadows: false,
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  }
+});
